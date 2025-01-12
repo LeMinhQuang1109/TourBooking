491ddf783ce24b6f23055dc96039c184ea7b4aaf
BuiDuyNguyen <bbq135084@gmail.com>
2025-01-13 02:11:01 +0700

    Bac Trung Nam

---
+++ src/main/java/com/example/demo/services/CategoryService.java
@@ -0,0 +1,24 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.Category;
+import com.example.demo.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Integer id) {
+        return categoryRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Category not found"));
+    }
+} 
